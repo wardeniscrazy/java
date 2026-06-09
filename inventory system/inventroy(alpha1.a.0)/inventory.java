aa45c41ffcbd3ba5312326692bfb39c32cc9e43c
wardeniscrazy <jitendra1982.jt@gmail.com>
2026-06-09 12:07:11 +0530

    Update inventory.java

--- inventory system/inventroy(alpha1.a.0)/inventory.java
+++ inventory system/inventroy(alpha1.a.0)/inventory.java
@@ -3,11 +3,11 @@ public class inventroy {
   public static void main(String[] args) { 
   
 
-   String [][] a = {{"picaxe  |","sword  |","axe         |","shovel"}
-    ,               {"diamond |","iron   |","gold        |","coal"}
-    ,               {"apple   |","bread  |","carrot      |","potato"}
-  ,                 {""}
-,                   {"Bow     |","Arrow  |","Fishing rod |","Water bucket"}};   
+   int [][] a = {{1,2,3,4,5}
+   ,{6,7,8,9,10}
+   ,{11,12,13,14,15}
+   ,{16,17,18,19,20}
+   ,{21,22,23,24,25}};
     for (int i = 0; i < a.length; i++) {
       for (int j = 0; j < a[i].length; j++) {
           System.out.print(a[i][j] + " ");
