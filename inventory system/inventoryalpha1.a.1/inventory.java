174e09a7d5018935b7d296fcbd7cdcb524c88aff
wardeniscrazy <jitendra1982.jt@gmail.com>
2026-06-09 12:05:02 +0530

    Update inventory.java

--- inventory system/inventoryalpha1.a.1/inventory.java
+++ inventory system/inventoryalpha1.a.1/inventory.java
@@ -1,1 +1,18 @@
+import java.util.Arrays;
+public class inventroy {
+  public static void main(String[] args) { 
+  
 
+   String [][] a = {{"picaxe  |","sword  |","axe         |","shovel"}
+    ,               {"diamond |","iron   |","gold        |","coal"}
+    ,               {"apple   |","bread  |","carrot      |","potato"}
+  ,                 {""}
+,                   {"Bow     |","Arrow  |","Fishing rod |","Water bucket"}};   
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < a[i].length; j++) {
+          System.out.print(a[i][j] + " ");
+      }
+ System.out.println();
+}
+}
+}
