2638aa77ca0aef8f972bc6e755471f76e954a6d6
wardeniscrazy <jitendra1982.jt@gmail.com>
2026-06-10 12:49:48 +0530

    Update inventory.java

--- inventory system/inventory(alpha1.a.2)/inventory.java
+++ inventory system/inventory(alpha1.a.2)/inventory.java
@@ -1,1 +1,47 @@
+import java.util.*;
+public class inventroy {
+  
+  public static void main(String[] args) { 
+
+
+Scanner sc = new Scanner(System.in);
+
+    while (true) {
+
+    System.out.println("1.> open inventoryz");
+    System.out.println("2.> close inventory");
+    // System.out.println("Inventory:");
+    // System.out.println("Inventory:");
+    // System.out.println("Inventory:");
+    // System.out.println("Inventory:");
+int open = sc.nextInt();
+if (open == 1) {
+    System.out.println("Inventory Opened");
+    
+    String [][] a = {{"picaxe  |","sword  |","axe         |","shovel"}
+    ,               {"diamond |","iron   |","gold        |","coal"}
+    ,               {"apple   |","bread  |","carrot      |","potato"}
+  ,                 {""}
+,                   {"Bow     |","Arrow  |","Fishing rod |","Water bucket"}};   
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < a[i].length; j++) {
+          System.out.print(a[i][j] + " ");
+      }
+ System.out.println();
+    
+}
+ 
+  }
+
+     if (open == 2) {
+    System.out.println("Inventory Closed");
+
+
+
+}
+   
+
+}
+  }
+}
 
